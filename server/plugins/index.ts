47f1dfed5f6e92f5a90453bb7f13893d1f50ed87
xiyou <yinpeng_1992@163.com>
2026-06-25 09:16:41 +0800

    feat: 添加Nitro插件以支持服务启动、停止hook方法，和挂载定时任务方法

---
+++ server/plugins/index.ts
@@ -0,0 +1,19 @@
+export default defineNitroPlugin((nitroApp) => {
+  // 这里可以写一些服务启动时的逻辑
+  console.log("=== Nitro服务已启动 ===");
+  scheduleTask(); // 启动定时任务
+
+  // 服务停止时的Hook
+  nitroApp.hooks.hook("close", async () => {
+    console.log("=== Nitro服务已停止 ===");
+  });
+});
+
+// 定义一个定时任务，每分钟执行一次
+function scheduleTask() {
+  let count = 0;
+  setInterval(() => {
+    count++;
+    console.log(`=== 定时任务执行中 (${count}) ===`);
+  }, 60000);
+}
